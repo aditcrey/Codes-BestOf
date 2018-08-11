f6fbd0a4c56134e9e7a3e64d0b432e848fd5e574
Aditya Thakre <aditcrey@gmail.com>
2018-08-11 11:25:54 +0530

    Update PrintMatrixInSpiral.cpp

--- PrintMatrixInSpiral.cpp
+++ PrintMatrixInSpiral.cpp
@@ -34,3 +34,44 @@ public:
         
     }
 };
+
+
+
+
+
+
+//====================================Solution #2 ============================================
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>> matrix) {
+        vector<int> ans;
+        int m  = matrix.size();
+        if(m==0) return ans;
+        int n = matrix[0].size();
+        
+        int l = 0,r=n-1,t = 0, b = m-1;
+        
+        while(l<=r && t<=b){
+            for(int i=l;i<=r;i++)
+                ans.push_back(matrix[t][i]);
+            t++;
+            
+            for(int i=t;i<=b;i++)
+                ans.push_back(matrix[i][r]);
+            r--;
+            
+            if(t<=b)
+            for(int i=r;i>=l;i--)
+                ans.push_back(matrix[b][i]);
+            b--;
+            
+            if(l<=r)
+            for(int i=b;i>=t;i--)
+                ans.push_back(matrix[i][l]);
+            l++;
+        }
+        
+        return ans;
+    }
+};
