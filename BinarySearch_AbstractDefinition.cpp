cb510e5bb208dd041c11d17023ee3064f1f36b32
Aditya Thakre <aditcrey@gmail.com>
2018-08-18 21:18:46 +0530

    Update BinarySearch_AbstractDefinition.cpp

--- BinarySearch_AbstractDefinition.cpp
+++ BinarySearch_AbstractDefinition.cpp
@@ -2,7 +2,7 @@ binary_search(int arr[], int n){ // F....FFFTTTTT....T //we want to find the fir
 	int l = 0, int r = n-1;
 	int mid;
 	
-	while(l<=r){
+	while(l<r){ //mind it// l<r and not l<=r because then it will go to infinite loop
 		if(P(mid)==false){ //for case of T.......TTTTTFFFFF......F  , replace false with true and finally output value at l-1 (which will be the last true value)
 			l= mid + 1;
 		}else{
